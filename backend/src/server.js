29b1cbca51e80542f70b637b8915b45009f2d6b3
RohitSamanta007 <137418771+RohitSamanta007@users.noreply.github.com>
2025-10-08 13:00:54 +0530

    Intial ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,26 @@
-console.log("Hello from server")
+console.log("Hello from server");
 
-import express from "express"
-import "dotenv/config"
-import authRoute from "./routes/auth.route.js"
-import messageRoute from "./routes/message.route.js"
+import express from "express";
+import "dotenv/config";
+import path from "path";
+
+import authRoute from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js";
 
 const port = process.env.PORT || 8000;
 const app = express();
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 
-app.listen(port, ()=> console.log("Server is running on port no : ", port));
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(port, () => console.log("Server is running on port no : ", port));
